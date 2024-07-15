f13a41ff02a0b9f5c18103151074f1237e91e2d6
Junhyunny <kang3966@naver.com>
2024-07-15 15:50:23 +0900

    Add an example of deployment backend and frontend on TAS

---
+++ backend/src/test/kotlin/action/in/blog/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package action.in.blog.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
